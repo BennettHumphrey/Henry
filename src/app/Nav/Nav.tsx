b49fcd2fd052fc9b8e9347e428498baca7f1875f
Ben Humphrey <bennetthumphrey98@gmail.com>
2024-10-16 13:36:27 -0500

    Fix build

--- src/app/Nav/Nav.tsx
+++ src/app/Nav/Nav.tsx
@@ -1,42 +1,29 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import MobileNav from "./MobileNav";
-import DesktopNav from "./DesktopNav";
+import React from "react";
 import Link from "next/link";
-import { useWindowSize } from "react-use";
 import { navOptions, navSocials } from "./data";
 
 const Nav = () => {
-  const { width } = useWindowSize();
-  const [windowWidth, setWindowWidth] = useState(700);
-
-  useEffect(() => {
-    setWindowWidth(width);
-  }, [width]);
-
   return (
     <div className="flex flex-col z-50 w-full border-b border-b-gray-300 fixed sm:flex-row">
-      {/* {windowWidth < 768 ? (
-        <MobileNav />
-      ) : (
-        <DesktopNav />
-      )} */}
       <div className="sm:basis-1/2">
-        <h1 className="text-2xl tracking-widest font-bold text-center sm:pl-4">HENRY</h1>
+        <h1 className="text-2xl tracking-widest font-bold text-center sm:pl-4">
+          HENRY
+        </h1>
       </div>
       <div className="flex justify-between w-full">
         <div className="flex items-center pl-4 gap-4">
           {navOptions.map((nav, index) => (
             <div className="group" key={index}>
               <Link href={nav.path}>{nav.title}</Link>
-              <div className='h-px w-0 group-hover:w-full duration-500 bg-text-dark'/>
+              <div className="h-px w-0 group-hover:w-full duration-500 bg-text-dark" />
             </div>
           ))}
         </div>
         <div className="flex gap-1 pr-2 items-center">
           {navSocials.map((social, index) => (
-            <Link className="" key={index} href={social.path} >
+            <Link className="" key={index} href={social.path}>
               <div className="fill-text-text-dark w-6 h-6">{social.icon}</div>
             </Link>
           ))}
